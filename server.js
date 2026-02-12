a251192112c1ebd3dcac7d4b086f213ffe371f27
Josh Kramer <josh.kramer@mail.mcgill.ca>
2026-02-11 21:13:11 -0500

    fixed

--- server.js
+++ server.js
@@ -1716,7 +1716,7 @@ if (ws.client !== "ios" || !receiverWs || receiverWs.client !== "ios") {
     // Create write stream for raw bytes
     const writeStream = fs.createWriteStream(filePath, {
       flags: "w",
-      highWaterMark: 16 * 1024 * 1024, // balanced throughput + backpressure
+      highWaterMark: 24 * 1024 * 1024, // slightly higher throughput, still bounded
     });
 
 
